427248b36edc6a8a09ef264964ad046fa5aa1352
singlerzhou <804738536@qq.com>
2023-04-01 09:25:00 +0800

    一、databinding简单使用

--- app/src/main/java/com/zxg/jetpack/application/MainActivity.java
+++ app/src/main/java/com/zxg/jetpack/application/MainActivity.java
@@ -1,14 +1,28 @@
 package com.zxg.jetpack.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.zxg.jetpack.application.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mDataBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+        User user = new User("张三", 28, 172, "女");
+        mDataBinding.setUser(user);
     }
 }
---
+++ app/src/main/java/com/zxg/jetpack/application/User.java
@@ -0,0 +1,47 @@
+package com.zxg.jetpack.application;
+
+public class User {
+    private String name;
+    private int age;
+    private long height;
+    private String sex;
+
+    public User(String name, int age, long height, String sex) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getHeight() {
+        return height;
+    }
+
+    public void setHeight(long height) {
+        this.height = height;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
